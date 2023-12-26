667281937060a22da12ef4b233373fde1cc73ef6
Niklas Stoffers <74815146+niklasstoffers@users.noreply.github.com>
2023-12-26 17:58:58 +0100

    Fix code quality issues

--- install-pre-commits.sh
+++ install-pre-commits.sh
@@ -12,18 +12,18 @@ fi
 
 echo "Please enter GitGuardian API key for ggshield pre-commit scan job"
 stty -echo
-read -p 'Gitguardian API key: ' gg_api_key; echo
+read -rp 'Gitguardian API key: ' gg_api_key; echo
 stty echo
 
 export_global=
 while ! [[ $export_global = "y" || $export_global = "N" ]]; do
-    read -p 'Do you want to add the API key as a global variable? Otherwise it has to be set each time environment variables are being reloaded [y/N]: ' export_global
+    read -rp 'Do you want to add the API key as a global variable? Otherwise it has to be set each time environment variables are being reloaded [y/N]: ' export_global
 done
 
 export GITGUARDIAN_API_KEY=$gg_api_key
 if [ $export_global = "y" ]; then
     echo "Exporting GitGuardian API key..."
-    echo "export GITGUARDIAN_API_KEY="$gg_api_key"">>~/.bashrc
+    echo "export GITGUARDIAN_API_KEY=$gg_api_key">>~/.bashrc
 else
     echo "GitGuardian API key will not be exported"
 fi
