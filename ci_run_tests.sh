509bc123060ce2f4043ef2e3faad0bd9ff1ee3d6
Niklas Stoffers <74815146+niklasstoffers@users.noreply.github.com>
2023-12-28 15:48:55 +0100

    Fix coverage running from wrong directory

--- ci_run_tests.sh
+++ ci_run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir coverage_report
-docker run --entrypoint /bin/sh -v "./tests:/tests" -v "./coverage_report:/coverage_report" gitlab-auto-approve -c \
-    "pip install -r /tests/requirements.txt && \
-     coverage run &&
+docker run --entrypoint /bin/sh -v "./tests:/tests" -v "./coverage_report:/coverage_report" --mount type=bind,source=./.coveragerc,target=/.coveragerc gitlab-auto-approve -c \
+    "cd / && \
+     pip install -r tests/requirements.txt && \
+     coverage run && \
      mv .coverage coverage_report"
